72f4e2d748cc9d7eaf29d382f183976ab2337c09
LuizJosse <luiznetto12@hotmail.com>
2025-09-04 23:02:04 -0300

    FIX

--- src/main/java/br/com/mariojp/solid/lspaccounts/BankService.java
+++ src/main/java/br/com/mariojp/solid/lspaccounts/BankService.java
@@ -2,11 +2,11 @@ package br.com.mariojp.solid.lspaccounts;
 
 public class BankService {
 
-    // Único método que resolve os dois testes:
+  
     public void processWithdrawal(Account acc, double amount) {
         if (acc instanceof Withdrawable) {
             ((Withdrawable) acc).withdraw(amount);
         }
-        // se não for Withdrawable (SavingsAccount), não faz nada
+      
     }
 }
